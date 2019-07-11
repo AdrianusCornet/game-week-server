14e3b42966186af50b3d544373e91ca5b60cd545
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-11 16:41:15 +0200

    send back user

--- auth/router.js
+++ auth/router.js
@@ -34,7 +34,8 @@ router.post('/login', (request, response, next) => {
                 }
                 else if (bcrypt.compareSync(password, entity.password)) {
                     response.send({
-                        jwt: toJWT({ userId: entity.id })
+                        jwt: toJWT({ userId: entity.id }),
+                        user:{entity}
                     })
                 }
                 else {
